f322d70d9cd50b4094cd4f9c30c60dc90adab14f
HemanthRaj2005 <2320030004@klh.edu.in>
2024-08-17 13:11:40 +0530

    Add files via upload

---
+++ Loggers/logg.java
@@ -0,0 +1,10 @@
+public class logg {
+    public logg() {
+    }
+ 
+    public static void main(String[] var0) {
+       Logger var1 = Logger.getInstance();
+       var1.log("logged in");
+    }
+ }
+ 
